a11a135d9cc0b29c228b1337e7075ea24229c142
Samuel Holgersson <samuel.holgersson@gmail.com>
2021-12-04 10:48:41 +0100

    Make input reader more general.
    Can now split by any arbitrary non word characters.

--- helper/input.py
+++ helper/input.py
@@ -1,9 +1,17 @@
+import re
+
+
 def read_input(day, part):
     read_input(day, part, [str])
 
-def read_input(day, part, type_and_args):
+
+def read_input(day, part, fun_and_args):
     with open(f'input/day{day:02d}_{part}.txt') as file:
         for line in file:
-            split_line = line.split(' ')
-            typed_split_line = [type(item, *args) for item, (type, *args) in zip(split_line, type_and_args)]
+            split_line = re.findall(r"[\w']+", line)
+            extended_fun_and_args = fun_and_args
+            while len(split_line) > len(extended_fun_and_args):
+                extended_fun_and_args += fun_and_args
+
+            typed_split_line = [fun(item, *args) for item, (fun, *args) in zip(split_line, fun_and_args)]
             yield typed_split_line
