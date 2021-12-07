9984c11a3b3b7494e02b03df125778dfc639f608
Samuel Holgersson <samuel.holgersson@gmail.com>
2021-12-07 12:09:34 +0100

    Optimize and cleanup day 7.

--- day07.py
+++ day07.py
@@ -1,45 +1,44 @@
 from helper.input import read_input
 import sys
-import numpy as np
+from collections import defaultdict
 
 
-def part1(input):
-    [crab_positions] = read_input(7, input, [(int,)])
-
-    crab_positions = np.array(crab_positions, dtype=int)
-
-    min_x = np.min(crab_positions)
-    max_x = np.max(crab_positions)
-
-    cost_of_going_to_x = np.array([0] * (max_x - min_x + 1), dtype=int)
-
-    for crab_position in crab_positions:
-        for i in range(min_x, max_x + 1):
-            cost_of_going_to_x[i] += np.abs(crab_position - i)
-
-    return np.min(cost_of_going_to_x)
+def cost1(a, b):
+    return abs(a - b)
 
 
-def cost(a, b):
-    distance = np.abs(a - b)
+def cost2(a, b):
+    distance = abs(a - b)
     return (distance * (distance + 1)) // 2
 
 
-def part2(input):
+def get_min_distance(input, distance_function):
     [crab_positions] = read_input(7, input, [(int,)])
 
-    crab_positions = np.array(crab_positions, dtype=int)
+    min_x = min(crab_positions)
+    max_x = max(crab_positions)
 
-    min_x = np.min(crab_positions)
-    max_x = np.max(crab_positions)
-
-    cost_of_going_to_x = np.array([0] * (max_x - min_x + 1), dtype=int)
+    crabs_at_positions = defaultdict(int)
 
     for crab_position in crab_positions:
-        for i in range(min_x, max_x + 1):
-            cost_of_going_to_x[i] += cost(crab_position, i)
+        crabs_at_positions[crab_position] += 1
 
-    return np.min(cost_of_going_to_x)
+    cost_of_going_to_x = [0] * (max_x - min_x + 1)
+
+    for crab_position, crabs_at_position in crabs_at_positions.items():
+        if crabs_at_position != 0:
+            for i in range(min_x, max_x + 1):
+                cost_of_going_to_x[i] += distance_function(crab_position, i) * crabs_at_position
+
+    return min(cost_of_going_to_x)
+
+
+def part1(input):
+    return get_min_distance(input, cost1)
+
+
+def part2(input):
+    return get_min_distance(input, cost2)
 
 
 if __name__ == '__main__':
